f75386312652524393b70ba3cb2a8a472ec11604
paul172v <paul172v@aol.com>
2025-04-24 04:56:15 +0100

    day6

---
+++ day6/task.py
@@ -0,0 +1,13 @@
+def my_function():
+    print('This')
+    print('Is')
+    print('A')
+    print('Function!!!')
+
+my_function()
+
+### while loop
+tick = 0
+while tick < 3:
+    print('tick')
+    tick += 1
