2a1a96e021a0e2e33d1d5dec7587aff490735b15
paul172v <paul172v@aol.com>
2025-04-24 04:16:08 +0100

    day5

---
+++ day5/task.py
@@ -0,0 +1,16 @@
+nums = [1, 2, 3]
+
+## total
+print(sum(nums))
+
+## loop total
+total = 0
+
+for num in nums: 
+    total += num
+
+print(total)
+
+## range
+for num in range(1, 10, 2):
+    print(num)
