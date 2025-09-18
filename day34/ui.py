2a4add914f12fa45d3981dd732d77de9dbf1bdc0
paul172v <paul172v@aol.com>
2025-09-18 23:49:37 +0100

    day34

---
+++ day34/ui.py
@@ -0,0 +1,57 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Some question text",
+            fill=THEME_COLOR,
+            font=("Ariel", 20, "italic"),
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(
+            image=true_image, highlightthickness=0, command=self.check_answer_true
+        )
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(
+            image=false_image, highlightthickness=0, command=self.check_answer_false
+        )
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def check_answer_true(self):
+        total_score = self.quiz.check_answer("true")
+        self.score_label.config(text=f"Score: {total_score}")
+        self.get_next_question()
+
+    def check_answer_false(self):
+        total_score = self.quiz.check_answer("false")
+        self.score_label.config(text=f"Score: {total_score}")
+        self.get_next_question()
